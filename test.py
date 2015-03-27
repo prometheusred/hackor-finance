58b12835d0d306a8427fc28635924f7c0204e2f5
Jonathan Streater <prometheusred@gmail.com>
2015-03-27 16:21:09 -0700

    functions for posgres tables to dataframes

--- test.py
+++ test.py
@@ -1,43 +1,53 @@
 import psycopg2 as pg
-import pandas.io.sql as psql
+#import psycopg2.extras as pge
+from psycopg2.extras import DictCursor
+
 from pandas.io.sql import read_sql
 #from pandas.io.sql import read_sql_table
 import pandas as pd
 import numpy as np
 
 #matplotlib
-
-
 conn = pg.connect("dbname=hackoregon user=jonathan.streater")
 cur = conn.cursor()
+
+
+def table_to_df(table):
+    sorted_table = sorted(table, key=lambda tup: tup[1])
+    column_names = [name[0] for name in cur.description]
+    labeled_table = map(lambda d: dict(zip(column_names, d)), sorted_table)
+    return pd.DataFrame(labeled_table)
+
+def get_table(table_name):
+    #global cur
+    query = "select * from " + table_name
+    cur.execute(query)
+    return cur.fetchall()
+
+
 
 #cur.execute("select sum(amount), filer from  raw_committee_transactions where contributor_payee = 'Fulcrum Political, LLC' group by filer order by sum(amount) desc;")
 #d = cur.execute("SELECT pg_size_pretty(pg_database_size('hackoregon'));")
 
-
+#cur.execute("select * from pg_catalog.pg_tables"
 cur.execute("""SELECT table_name FROM information_schema.tables
        WHERE table_schema = 'public'""")
-
-#cur.execute("select * from pg_catalog.pg_tables"
-
 table_names = cur.fetchall()
 table_names = map(lambda x: x[0], table_names)
 
 #cur.execute("select * from ac_grass_roots_in_state")
-root_query = "select * from "
-
-cur.execute(root_query + table_names[0])
-
-table = cur.fetchall()
 
 
-###df = read_sql(())
+se_transactions = table_to_df(get_table('working_transactions'))
 
-#df = read_sql(('select "*" from "ac_grass_roots_in_state" '), cur)
-                     #'where "Timestamp" BETWEEN %(dstart)s AND %(dfinish)s'),
-#df = pd.read_sql("SELECT * from raw_committee_transactions", cur)
+se_candidate_committees = table_to_df(get_table('working_candidate_committees'))
+#se_transactions = pd.Series(sorted_table)
 
-#df = read_sql_table(cur,cur)
+se_candidate_filings = table_to_df(get_table('working_candidate_filings'))
+
+se_working_committees = table_to_df(get_table('working_committees'))
+
+
 
 #cur.execute("select column_name, data_type, character_maximum_length from INFORMATION_SCHEMA.COLUMNS where table_name = '';")
 #print cur.fetchall()
@@ -49,8 +59,14 @@ cur.close()
 conn.close()
 
 
-# put a table into dataframe
+# write script to transfer given table to series DONE
+# script to transfer tables to data frames (can also do series)
 
-# find out data schema of all tables
+# research algorithms that are relevant
 
-# put all tables into dataframes
+# explore data -- figure out what is where
+#network analysis?
+#influence of variables?
+#adding rows for wins?  what can you predict?
+
+# help jon with d3
